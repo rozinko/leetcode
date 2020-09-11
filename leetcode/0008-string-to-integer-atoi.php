ef27262f14d94b7e23e9cbe90878886c91b4fd05
Roman Rozinko <r.rozinko@gmail.com>
2020-09-11 22:55:17 +0300

    add leetcode #8

---
+++ leetcode/0008-string-to-integer-atoi.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * 8. String to Integer (atoi)
+ * https://leetcode.com/problems/string-to-integer-atoi/
+ */
+
+class Solution
+{
+    /**
+     * @param String $str
+     * @return Integer
+     */
+    function myAtoi($str)
+    {
+        $str = trim($str);
+        $int = (int)(strpos($str, ' ') ? substr($str, 0, strpos($str, ' ')) : $str);
+        if (preg_match('/^([\-\+]?[0-9]+)\D.*/', $str, $match)) $int = (int)$match[1];
+        if ($int >= pow(2, 31)) return pow(2, 31) - 1;
+        if ($int < pow(-2, 31)) return pow(-2, 31);
+        return $int;
+    }
+}
