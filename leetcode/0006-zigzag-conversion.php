19a91aa173db25b8717b50dd96fa4d3d35251149
Roman Rozinko <r.rozinko@gmail.com>
2020-09-15 01:46:11 +0300

    add leetcode #6 at php

---
+++ leetcode/0006-zigzag-conversion.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * 6. ZigZag Conversion
+ * https://leetcode.com/problems/zigzag-conversion/
+ */
+
+class Solution
+{
+    /**
+     * @param String $s
+     * @param Integer $numRows
+     * @return String
+     */
+    function convert($s, $numRows)
+    {
+        if ($numRows <= 1) return $s;
+
+        $str = [];
+        $cycle = $numRows * 2 - 2;
+
+        for ($i = 1; $i <= strlen($s); $i++) {
+            $j = $i % $cycle == 0 ? $cycle : $i % $cycle;
+            if ($j > $numRows) $j = $numRows * 2 - $j;
+            if (isset($str[$j])) {
+                $str[$j] .= $s[$i - 1];
+            } else {
+                $str[$j] = $s[$i - 1];
+            }
+        }
+
+        return implode('', $str);
+    }
+}
