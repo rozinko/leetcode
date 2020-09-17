7d65cfef88a7c32ee8d4d1841525d40aea56968e
Roman Rozinko <r.rozinko@gmail.com>
2020-09-17 16:56:04 +0300

    fix leetcode #1041 at php

--- leetcode/1041-robot-bounded-in-circle.php
+++ leetcode/1041-robot-bounded-in-circle.php
@@ -5,25 +5,33 @@
  * https://leetcode.com/problems/robot-bounded-in-circle/
  */
 
-class Solution {
-
+class Solution
+{
     /**
      * @param String $instructions
      * @return Boolean
      */
-    function isRobotBounded($instructions) {
+    function isRobotBounded($instructions)
+    {
         // x, y, dx, dy
         $x = $y = $dx = 0;
         $dy = 1;
 
-        for($i = 0; $i < 4; $i ++) {
-            for($j = 0; $j < strlen($instructions); $j++)
-                switch($instructions[$j]) {
-                    case 'L': list($dx, $dy) = [-$dy, $dx]; break;
-                    case 'R': list($dx, $dy) = [$dy, -$dx]; break;
-                    case 'G': $x += $dx; $y += $dy; break;
+        for ($i = 0; $i < 4; $i++) {
+            for ($j = 0; $j < strlen($instructions); $j++)
+                switch ($instructions[$j]) {
+                    case 'L':
+                        list($dx, $dy) = [-$dy, $dx];
+                        break;
+                    case 'R':
+                        list($dx, $dy) = [$dy, -$dx];
+                        break;
+                    case 'G':
+                        $x += $dx;
+                        $y += $dy;
+                        break;
                 }
-            if($x==0 && $y==0 && $dx==0 && $dy==1) return true;
+            if ($x == 0 && $y == 0 && $dx == 0 && $dy == 1) return true;
         }
         return false;
     }
