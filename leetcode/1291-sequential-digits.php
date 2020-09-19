f44a972c12d291b4c922e83d9caaeb8d70a3e624
Roman Rozinko <r.rozinko@gmail.com>
2020-09-19 10:22:08 +0300

    add leetcode #1291 at php

---
+++ leetcode/1291-sequential-digits.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * 1291. Sequential Digits
+ * https://leetcode.com/problems/sequential-digits/
+ */
+
+class Solution
+{
+    /**
+     * @param Integer $low
+     * @param Integer $high
+     * @return Integer[]
+     */
+    function sequentialDigits($low, $high)
+    {
+        $s = '123456789';
+
+        $minDigits = strlen($low);
+        $maxDigits = strlen($high);
+
+        $nums = [];
+
+        for ($digits = $minDigits; $digits <= $maxDigits; $digits++) {
+            for ($start = 0; $start < 9; $start++) {
+                $num = (int)substr($s, $start, $digits);
+                if (strlen($num) < $digits) continue 2;
+                if ($num < $low) continue;
+                if ($num > $high) return $nums;
+                $nums[] = $num;
+            }
+        }
+
+        return $nums;
+    }
+}
