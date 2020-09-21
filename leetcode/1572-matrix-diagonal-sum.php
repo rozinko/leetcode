634fe8ca4980461024b68daeb5fb03a16a414efe
Roman Rozinko <r.rozinko@gmail.com>
2020-09-21 15:26:46 +0300

    fix leetcode #1572 at php

--- leetcode/1572-matrix-diagonal-sum.php
+++ leetcode/1572-matrix-diagonal-sum.php
@@ -13,9 +13,7 @@ class Solution
      */
     function diagonalSum($mat)
     {
-        $sum = 0;
-        $max = count($mat) - 1;
-        for ($i = 0; $i <= $max; $i++)
+        for ($i = 0, $sum = 0, $max = count($mat) - 1; $i <= $max; $i++)
             $sum += $mat[$i][$i] + ($i != $max - $i ? $mat[$i][$max - $i] : 0);
         return $sum;
     }
