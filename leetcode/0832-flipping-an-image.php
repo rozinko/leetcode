b252c9c52cd87b5d58583228baf9afa54425e4cd
Roman Rozinko <r.rozinko@gmail.com>
2020-11-10 17:45:22 +0300

    update #832 at php

--- leetcode/0832-flipping-an-image.php
+++ leetcode/0832-flipping-an-image.php
@@ -15,6 +15,19 @@ class Solution
      */
     function flipAndInvertImage($A)
     {
+        return array_map(function ($row) {
+            return array_map(function ($v) {
+                return 1 - $v;
+            }, array_reverse($row));
+        }, $A);
+    }
+
+    /**
+     * @param Integer[][] $A
+     * @return Integer[][]
+     */
+    function flipAndInvertImage2($A)
+    {
         for ($i = 0; $i < count($A); $i++)
             for ($j = 0; $j <= ceil(count($A[$i]) / 2 - 1); $j++)
                 list($A[$i][$j], $A[$i][count($A[$i]) - $j - 1]) = [1 - $A[$i][count($A[$i]) - $j - 1], 1 - $A[$i][$j]];
