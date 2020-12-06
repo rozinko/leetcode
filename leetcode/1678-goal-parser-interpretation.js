b841ed4a9591fe78f46324fb568178e9286f52b6
Roman Rozinko <r.rozinko@gmail.com>
2020-12-06 11:12:53 +0300

    adddescription to leetcode #1678 at php, js, swift

--- leetcode/1678-goal-parser-interpretation.js
+++ leetcode/1678-goal-parser-interpretation.js
@@ -1,6 +1,8 @@
 /**
  * 1678. Goal Parser Interpretation
  * https://leetcode.com/problems/goal-parser-interpretation/
+ *
+ * https://leetcode.com/problems/goal-parser-interpretation/discuss/961778/PHP-JavaScript-Swift-One-line-100-fast-with-explanation
  */
 
 /**
