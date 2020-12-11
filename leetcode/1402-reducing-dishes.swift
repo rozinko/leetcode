7f35bd8c158479b12fd83e4474457f0acfe2110e
Roman Rozinko <r.rozinko@gmail.com>
2020-12-12 01:40:44 +0300

    modify leetcode #1402 at swift

--- leetcode/1402-reducing-dishes.swift
+++ leetcode/1402-reducing-dishes.swift
@@ -5,23 +5,12 @@
 
 class Solution {
     func maxSatisfaction(_ satisfaction: [Int]) -> Int {
-        var satisfaction = satisfaction.sorted()
-        let positiveSum = satisfaction.filter{$0 >= 0}.reduce(0, +)
-        var negLine = 0, diff = 0, prevDiff = 0
-
-        var i = satisfaction.filter{$0 < 0}.count
-        while i > 0 {
-            i -= 1
-            negLine += satisfaction[i]
-            diff += negLine + positiveSum
-            if diff >= prevDiff {
-                prevDiff = diff
-            } else {
-                i += 1
-                break
-            }
+        var max = 0, line = 0
+        for value in satisfaction.sorted(by: >) where line + value > 0 {
+            line += value
+            max += line
         }
 
-        return satisfaction[i...].enumerated().reduce(0, {$0 + $1.element * ($1.offset + 1)})
+        return max
     }
 }
