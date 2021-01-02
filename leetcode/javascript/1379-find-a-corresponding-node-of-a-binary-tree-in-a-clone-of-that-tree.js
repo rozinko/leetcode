ddffcfda91e767b98727107338df6ecfcd17e5b3
Roman Rozinko <r.rozinko@gmail.com>
2021-01-02 13:07:13 +0300

    add leetcode #1379 at javascript

---
+++ leetcode/javascript/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.js
@@ -0,0 +1,24 @@
+/**
+ * 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree
+ * https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} original
+ * @param {TreeNode} cloned
+ * @param {TreeNode} target
+ * @return {TreeNode}
+ */
+
+var getTargetCopy = function (original, cloned, target) {
+    if (original == null) return false
+    if (original === target) return cloned
+    return getTargetCopy(original.left, cloned.left, target) || getTargetCopy(original.right, cloned.right, target)
+};
