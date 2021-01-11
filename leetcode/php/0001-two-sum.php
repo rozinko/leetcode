bb5285597ce4624a3e234569a180d3a2c0912400
Roman Rozinko <r.rozinko@gmail.com>
2021-01-12 00:38:32 +0300

    modify leetcode #1 at php

--- leetcode/php/0001-two-sum.php
+++ leetcode/php/0001-two-sum.php
@@ -5,22 +5,19 @@
  * https://leetcode.com/problems/two-sum/
  */
 
-class Solution
-{
+class Solution {
     /**
      * @param Integer[] $nums
      * @param Integer $target
      * @return Integer[]
      */
-    function twoSum($nums, $target)
-    {
+    function twoSum($nums, $target) {
         $map = [];
-
-        foreach ($nums as $i => $num)
-            if (!isset($map[$target - $num])) {
+        foreach($nums as $i => $num) {
+            if(isset($map[$target-$num]))
+                return [$map[$target-$num], $i];
+            else
                 $map[$num] = $i;
-            } else {
-                return [$map[$target - $num], $i];
-            }
+        }
     }
 }
