b0a57ef7aa2fee75b26815d4d3660983c67249b4
Roman Rozinko <r.rozinko@gmail.com>
2021-01-12 00:45:14 +0300

    modify leetcode #2 at php

--- leetcode/php/0002-add-two-numbers.php
+++ leetcode/php/0002-add-two-numbers.php
@@ -18,7 +18,6 @@
  */
 class Solution
 {
-
     /**
      * @param ListNode $l1
      * @param ListNode $l2
@@ -27,14 +26,15 @@ class Solution
     function addTwoNumbers($l1, $l2)
     {
         $head = $current = new ListNode();
+        $valNext = 0;
 
-        $val = $valNext = 0;
         while ($l1 || $l2 || $valNext) {
             $val = ($l1->val ?? 0) + ($l2->val ?? 0) + $valNext;
             $valNext = $val >= 10 ? 1 : 0;
 
             $current->next = new ListNode($val % 10);
             $current = $current->next;
+
             $l1 = $l1->next;
             $l2 = $l2->next;
         }
