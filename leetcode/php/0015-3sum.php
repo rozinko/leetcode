43ca1947418e7c190142af55d22fc3c94d194c81
Roman Rozinko <r.rozinko@gmail.com>
2021-01-12 17:37:28 +0300

    modify leetcode #15 at php

--- leetcode/php/0015-3sum.php
+++ leetcode/php/0015-3sum.php
@@ -15,43 +15,26 @@ class Solution
     {
         if (count($nums) < 3) return [];
 
-        $results = [];
+        $result = [];
+        sort($nums);
 
-        $positive = array_values(array_filter($nums, function ($v) {
-            return $v > 0;
-        }));
-        $negative = array_values(array_filter($nums, function ($v) {
-            return $v < 0;
-        }));
-        $zeros = count(array_keys($nums, 0));
+        for ($i = 0; $i < count($nums) - 2; $i++)
+            if ($i == 0 || $nums[$i] > $nums[$i - 1]) {
+                $l = $i + 1;
+                $r = count($nums) - 1;
+                while ($l < $r) {
+                    $sum = $nums[$i] + $nums[$l] + $nums[$r];
+                    if ($sum > 0) {
+                        $r--;
+                    } elseif ($sum < 0) {
+                        $l++;
+                    } else {
+                        $result[] = [$nums[$i], $nums[$l], $nums[$r]];
+                        $l++;
+                    }
+                }
+            }
 
-        $keys = array_flip($nums);
-
-        sort($positive);
-        sort($negative);
-
-        // two positive and negative
-        for ($i = 0; $i < count($positive) - 1; $i++)
-            for ($j = $i + 1; $j < count($positive); $j++)
-                if (isset($keys[($positive[$i] + $positive[$j]) * -1]))
-                    $results[] = [($positive[$i] + $positive[$j]) * -1, $positive[$i], $positive[$j]];
-
-        // two negative and positive
-        for ($i = 0; $i < count($negative) - 1; $i++)
-            for ($j = $i + 1; $j < count($negative); $j++)
-                if (isset($keys[($negative[$i] + $negative[$j]) * -1]))
-                    $results[] = [$negative[$i], $negative[$j], ($negative[$i] + $negative[$j]) * -1];
-
-        // negative, positive and zero
-        if ($zeros)
-            for ($i = 0; $i < count($negative); $i++)
-                if (isset($keys[$negative[$i] * -1]))
-                    $results[] = [$negative[$i], $negative[$i] * -1, 0];
-
-        // three zeros
-        if ($zeros >= 3)
-            $results[] = [0, 0, 0];
-
-        return array_unique($results, SORT_REGULAR);
+        return array_unique($result, SORT_REGULAR);
     }
 }
