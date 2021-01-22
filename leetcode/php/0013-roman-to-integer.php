84a9db0525da05ccd050e96f6c0132565cacfebd
Roman Rozinko <r.rozinko@gmail.com>
2021-01-22 12:49:46 +0300

    modify leetcode #13 at php

--- leetcode/php/0013-roman-to-integer.php
+++ leetcode/php/0013-roman-to-integer.php
@@ -14,45 +14,18 @@ class Solution
     function romanToInt($s)
     {
         $sum = 0;
-
-        $prices2x = [
-            'IV' => 4,
-            'IX' => 9,
-            'XL' => 40,
-            'XC' => 90,
-            'CD' => 400,
-            'CM' => 900,
-        ];
-
-        $prices = [
-            'I' => 1,
-            'V' => 5,
-            'X' => 10,
-            'L' => 50,
-            'C' => 100,
-            'D' => 500,
-            'M' => 1000,
-        ];
-
-        while ($s != '') {
-            if (strlen($s) >= 2) {
-                foreach ($prices2x as $roman => $value) {
-                    if ($s[0] . $s[1] == $roman) {
-                        $s = substr($s, 2);
-                        $sum += $value;
-                        continue 2;
-                    }
-                }
+        $prices = ['I' => 1, 'V' => 5, 'X' => 10, 'L' => 50, 'C' => 100, 'D' => 500, 'M' => 1000];
+        $prices2x = ['IV' => 4, 'IX' => 9, 'XL' => 40, 'XC' => 90, 'CD' => 400, 'CM' => 900];
+        $i = 0;
+        $len = strlen($s);
+        while ($i < $len) {
+            while ($i < $len - 1 && isset($prices2x[$s[$i] . $s[$i + 1]])) {
+                $sum += $prices2x[$s[$i++] . $s[$i++]];
             }
-            foreach ($prices as $roman => $value) {
-                if ($s[0] == $roman) {
-                    $s = substr($s, 1);
-                    $sum += $value;
-                    continue 2;
-                }
+            if ($i < $len) {
+                $sum += $prices[$s[$i++]];
             }
         }
-
         return $sum;
     }
 }
