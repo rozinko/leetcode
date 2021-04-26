feabb69df8b3f2a38f78baa280dfe80a3350996d
Roman Rozinko <r.rozinko@gmail.com>
2021-04-26 21:18:02 +0300

    add leetcode #700 at php

---
+++ leetcode/php/0700-search-in-a-binary-search-tree.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * 700. Search in a Binary Search Tree
+ * https://leetcode.com/problems/search-in-a-binary-search-tree/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution
+{
+    /**
+     * @param TreeNode $root
+     * @param Integer  $val
+     * @return TreeNode
+     */
+    function searchBST($root, $val)
+    {
+        while ($root && $root->val !== $val)
+            $root = $val < $root->val ? $root->left : $root->right;
+        return $root;
+    }
+}
