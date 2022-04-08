5d44b6570655ad7c3d39c97ede70e139b0bd30a0
Roman Rozinko <r.rozinko@gmail.com>
2022-04-08 22:19:37 +0300

    add leetcode #977 solution #2 at php

--- leetcode/php/0977-squares-of-a-sorted-array.php
+++ leetcode/php/0977-squares-of-a-sorted-array.php
@@ -5,6 +5,7 @@
  * https://leetcode.com/problems/squares-of-a-sorted-array/
  */
 
+// Solution with array_map and sort
 class Solution
 {
     /**
@@ -18,5 +19,53 @@ class Solution
         }, $nums);
         sort($nums);
         return $nums;
+    }
+}
+
+// Solution with two pointers
+class Solution2
+{
+    /**
+     * @param Integer[] $nums
+     * @return Integer[]
+     */
+    function sortedSquares($nums)
+    {
+        $l = -1;
+        $r = $length = count($nums);
+
+        $result = [];
+
+        while ($l + 1 < $r) {
+            $m = ceil(($l + $r) / 2);
+            if ($nums[$m] >= 0) $r = $m; else $l = $m;
+        }
+
+        while ($l >= 0 || $r < $length) {
+            if (isset($nums[$l])) {
+                if (isset($nums[$r])) {
+                    // l and r actual
+                    if ($nums[$r] >= abs($nums[$l])) {
+                        // add l
+                        $result[] = $nums[$l] * $nums[$l];
+                        $l--;
+                    } else {
+                        // add r
+                        $result[] = $nums[$r] * $nums[$r];
+                        $r++;
+                    }
+                } else {
+                    // only l
+                    $result[] = $nums[$l] * $nums[$l];
+                    $l--;
+                }
+            } else {
+                // only r
+                $result[] = $nums[$r] * $nums[$r];
+                $r++;
+            }
+        }
+
+        return $result;
     }
 }
