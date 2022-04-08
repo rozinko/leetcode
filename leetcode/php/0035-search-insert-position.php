5e01177433548c50b20df485fa35c11a15a74c5b
Roman Rozinko <r.rozinko@gmail.com>
2022-04-08 21:29:13 +0300

    add leetcode #35 solution #2 at php

--- leetcode/php/0035-search-insert-position.php
+++ leetcode/php/0035-search-insert-position.php
@@ -9,7 +9,7 @@ class Solution
 {
     /**
      * @param Integer[] $nums
-     * @param Integer $target
+     * @param Integer   $target
      * @return Integer
      */
     function searchInsert($nums, $target)
@@ -18,5 +18,31 @@ class Solution
             if ($target > $nums[$i - 1])
                 return $i;
         return 0;
+    }
+}
+
+
+class Solution2
+{
+    /**
+     * @param Integer[] $nums
+     * @param Integer   $target
+     * @return Integer
+     */
+    function searchInsert($nums, $target)
+    {
+        $l = 0;
+        $r = count($nums) - 1;
+
+        if ($target <= $nums[$l]) return 0;
+        if ($target > $nums[$r]) return $r + 1;
+
+        while ($l + 1 < $r) {
+            $m = ceil(($l + $r) / 2);
+            if ($target > $nums[$m]) $l = $m;
+            if ($target <= $nums[$m]) $r = $m;
+        }
+
+        return $r;
     }
 }
