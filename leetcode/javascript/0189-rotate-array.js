ffb304ebbe7be92b0c0cd08ada979bd42d2e6994
Roman Rozinko <r.rozinko@gmail.com>
2022-11-03 13:17:43 +0300

    add leetcode #189 at javascript

---
+++ leetcode/javascript/0189-rotate-array.js
@@ -0,0 +1,14 @@
+/**
+ * 189. Rotate Array
+ * https://leetcode.com/problems/rotate-array/
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  k %= nums.length;
+  nums.unshift(...nums.splice(nums.length - k));
+};
