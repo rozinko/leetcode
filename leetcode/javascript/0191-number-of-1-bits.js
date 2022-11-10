e04c0b848c532a4da357e53c507c8250f197a2cb
Roman Rozinko <r.rozinko@gmail.com>
2022-11-10 14:23:39 +0300

    modify leetcode #191 at javascript

--- leetcode/javascript/0191-number-of-1-bits.js
+++ leetcode/javascript/0191-number-of-1-bits.js
@@ -8,15 +8,26 @@
  * @return {number}
  */
 var hammingWeight = function (n) {
-    let m = 2147483648, ans = 0
-    while (m) {
-        if (n & m) ans++
-        m >>>= 1
-    }
-    return ans
+  let m = 2147483648,
+    ans = 0;
+  while (m) {
+    if (n & m) ans++;
+    m >>>= 1;
+  }
+  return ans;
 };
 
-
 var hammingWeight2 = function (n) {
-    return n.toString(2).split('').filter(a => a === '1').length
+  return n
+    .toString(2)
+    .split('')
+    .filter((a) => a === '1').length;
+};
+
+var hammingWeight3 = function (n) {
+  return n
+    .toString(2)
+    .split('')
+    .map((a) => parseInt(a))
+    .reduce((a, b) => a + b);
 };
