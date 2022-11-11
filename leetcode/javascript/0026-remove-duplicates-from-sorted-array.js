301f74ee246ed32360a60196da7116e03fed936c
Roman Rozinko <r.rozinko@gmail.com>
2022-11-11 16:09:59 +0300

    modify leetcode #26 at javascript

--- leetcode/javascript/0026-remove-duplicates-from-sorted-array.js
+++ leetcode/javascript/0026-remove-duplicates-from-sorted-array.js
@@ -8,10 +8,8 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-    let len = nums.length;
-    if (len > 1)
-        for (let i = len - 1; i; i--)
-            if (nums[i] === nums[i - 1])
-                nums.splice(i, 1);
-    return nums.length;
+  for (let i = nums.length - 1; i; i--)
+    if (nums[i] === nums[i - 1]) nums.splice(i, 1);
+
+  return nums.length;
 };
