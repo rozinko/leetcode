191c9deacf24271e949659dc698f2e93a347affa
Roman Rozinko <r.rozinko@gmail.com>
2023-02-06 13:04:45 +0300

    add leetcode #1470 at javascript

---
+++ leetcode/javascript/1470-shuffle-the-array.js
@@ -0,0 +1,15 @@
+/**
+ * 1470. Shuffle the Array
+ * https://leetcode.com/problems/shuffle-the-array/
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function (nums, n) {
+  let result = [];
+  for (let i = 0; i < n; i++) result.push(nums[i], nums[i + n]);
+  return result;
+};
