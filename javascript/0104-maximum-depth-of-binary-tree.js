d20c3dc01aa0ce27071ef45fc584f2a9e4ea62b1
Roman Rozinko <r.rozinko@gmail.com>
2023-02-16 11:18:51 +0300

    add #104 at javascript

---
+++ javascript/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,20 @@
+/**
+ * 104. Maximum Depth of Binary Tree
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  return root ? Math.max(maxDepth(root.left), maxDepth(root.right)) + 1 : 0;
+};
